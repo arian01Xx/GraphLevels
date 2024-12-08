27d41148b24164e6e64ad8139a1af5bdc6b97412
Arian Joseph <arian_joseph@outlook.es>
2024-12-08 00:11:05 -0500

    levels

---
+++ levels.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int low=1, high=*max_element(nums.begin(), nums.end());
+        while(low<high){
+            int mid=low+(high-low)/2;
+            int ops=0;
+            for(int n: nums){
+                if((ops+=(n-1)/mid) > maxOperations) break;
+            }
+            ops<=maxOperations ? high=mid : low=mid+1;
+        }
+        return high;
+    }
+};
+
+int main(){
+    return 0;
+}
